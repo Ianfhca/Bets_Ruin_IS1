13567222830ed818fc3b9bacfee421ed0091c329
Ian <63926745+Ianfhca@users.noreply.github.com>
2022-02-28 14:14:46 +0100

    Clase Login
    Prueba

---
+++ src/main/java/gui/Login.java
@@ -0,0 +1,17 @@
+package gui;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class Login extends JFrame {
+	public Login() {
+		
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
