86a44e62403b5f931a2ad4d23bfcb0decd24644f
samuelguzmanajala <53486599+samuelguzmanajala@users.noreply.github.com>
2022-03-07 23:09:35 +0100

    UserId

--- src/main/java/domain/ValueObjects/UserId.java
+++ src/main/java/domain/ValueObjects/UserId.java
@@ -1,9 +1,8 @@
 package domain.ValueObjects;
 
-import modules.Shared.domain.ValueObjects.ValueObjectUUID;
 
 import javax.persistence.Embeddable;
-import java.util.UUID;
+
 @Embeddable
 public class UserId extends ValueObjectUUID {
     public UserId(){
