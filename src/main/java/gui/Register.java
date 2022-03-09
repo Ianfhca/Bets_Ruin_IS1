95ed2af447273266c605985ada32243b8bc0a50e
Ikoi99 <99804219+Ikoi99@users.noreply.github.com>
2022-03-09 17:27:58 +0100

    Prueba

--- src/main/java/gui/Register.java
+++ src/main/java/gui/Register.java
@@ -151,6 +151,7 @@ public class Register extends JFrame{
 			monthNames.addElement("October");
 			monthNames.addElement("November");
 			monthNames.addElement("December");
+			monthNames.addElement("December");
 			month.setSelectedIndex(0);
 		
 		lblDay = new JLabel("Day:");
